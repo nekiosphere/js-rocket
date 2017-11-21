6c9178d3faf3718f2a2c21a2f55db113adf807c1
nekiosphere <33814337+nekiosphere@users.noreply.github.com>
2017-11-20 19:30:26 -0500

    made rocket

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+
+    // changing class name 
+    if (currentClassName == 'cool') {
+      document.getElementById('cool').className = 'cool red';
+    } else {
+        document.getElementById('cool').className = 'cool';
+    }
+}
+
+var sayMyName = function (name) {
+  alert('My name is: '+name);
+}
+
 var car = {
   make: 'VW',
   type: 'polo',
